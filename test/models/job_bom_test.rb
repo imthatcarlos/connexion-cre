5510ab61f1618c1990c7c19c0ccbe2aa2da50606
Mike Chau <mike@chau.io>
2016-08-27 12:42:23 -0500

    Remove test

--- test/models/job_bom_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class JobBomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/job_cust_change_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class JobCustChangeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
