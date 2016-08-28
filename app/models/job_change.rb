419c057e2de7265edbedb537bfd729fe466c921d
Mike Chau <mike@chau.io>
2016-08-27 23:16:37 -0500

    Update job change

--- app/models/job_change.rb
+++ app/models/job_change.rb
@@ -1,5 +1,5 @@
 class JobChange < ApplicationRecord
   self.table_name = "job_change"
 
-  # belongs_to :component, foreign_key: :order_number, primary_key: :transaction_id, class_name: "JobComponent"
+  belongs_to :component, foreign_key: :transaction_id, primary_key: :order_number, class_name: "JobComponent"
 end
